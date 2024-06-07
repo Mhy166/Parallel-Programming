edfcb1556cfad8e2b41df7629b809fec662171b3
Ma Haoyi <162128741+Mhy166@users.noreply.github.com>
2024-06-07 12:18:32 +0800

    GPU Lab！

---
+++ GPU_Lab/GPU-cpp/03练习.cpp
@@ -0,0 +1,63 @@
+﻿%% writefile lab / usm_lab.cpp
+//==============================================================
+// Copyright © Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+// =============================================================
+#include <sycl/sycl.hpp>
+using namespace sycl;
+
+static const int N = 1024;
+
+int main() {
+    queue q;
+    std::cout << "Device : " << q.get_device().get_info<info::device::name>() << "\n";
+
+    // 初始化主机上的两个数组
+    int* data1 = static_cast<int*>(malloc(N * sizeof(int)));
+    int* data2 = static_cast<int*>(malloc(N * sizeof(int)));
+    for (int i = 0; i < N; i++) {
+        data1[i] = 25;
+        data2[i] = 49;
+    }
+
+    // STEP 1: 创建 USM 设备分配
+    int* device_data1 = malloc_device<int>(N, q);
+    int* device_data2 = malloc_device<int>(N, q);
+
+    // STEP 2: 将 data1 和 data2 复制到 USM 设备分配中
+    q.memcpy(device_data1, data1, N * sizeof(int)).wait();
+    q.memcpy(device_data2, data2, N * sizeof(int)).wait();
+
+    // STEP 3: 编写内核代码，将 device_data1 的每个元素更新为其平方根
+    q.parallel_for(N, [=](auto i) {
+        device_data1[i] = std::sqrt(device_data1[i]);
+        });
+
+    // STEP 4: 编写内核代码，将 device_data2 的每个元素更新为其平方根
+    q.parallel_for(N, [=](auto i) {
+        device_data2[i] = std::sqrt(device_data2[i]);
+        });
+
+    // STEP 5: 编写内核代码，将 device_data2 的每个元素加到 device_data1 中
+    q.parallel_for(N, [=](auto i) {
+        device_data1[i] += device_data2[i];
+        }).wait();
+
+        // STEP 6: 将 device_data1 复制回主机上的 data1
+        q.memcpy(data1, device_data1, N * sizeof(int)).wait();
+
+        // 验证结果
+        int fail = 0;
+        for (int i = 0; i < N; i++) if (data1[i] != 12) { fail = 1; break; }
+        if (fail == 1) std::cout << " FAIL"; else std::cout << " PASS";
+        std::cout << "\n";
+
+        // STEP 7: 释放 USM 设备分配
+        free(device_data1, q);
+        free(device_data2, q);
+        free(data1);
+        free(data2);
+
+        return 0;
+}
---
+++ GPU_Lab/GPU-cpp/08练习.cpp
@@ -0,0 +1,55 @@
+﻿%% writefile lab / reduction_lab.cpp
+//==============================================================
+// Copyright © Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+// =============================================================
+#include <sycl/sycl.hpp>
+
+using namespace sycl;
+
+static constexpr size_t N = 1024; // global size
+static constexpr size_t B = 128; // work-group size
+
+int main() {
+    //# setup queue with default selector
+    queue q;
+    std::cout << "Device : " << q.get_device().get_info<info::device::name>() << "\n";
+
+    //# initialize data array using usm
+    auto data = malloc_shared<int>(N, q);
+    for (int i = 0; i < N; i++) data[i] = i;
+
+    //# implicit USM for writing min and max value
+    int* min = malloc_shared<int>(1, q);
+    int* max = malloc_shared<int>(1, q);
+    *min = 0;
+    *max = 0;
+
+    //# STEP 1 : Create reduction objects for computing min and max
+
+    //# YOUR CODE GOES HERE
+    auto min_reduction = reduction(min, sycl::ext::oneapi::minimum<>());
+    auto max_reduction = reduction(max, sycl::ext::oneapi::maximum<>());
+
+
+
+    //# Reduction Kernel get min and max
+    q.submit([&](handler& h) {
+        // 第二步：添加带有最小值和最大值归约对象的parallel_for
+        h.parallel_for(nd_range<1>{N, B}, min_reduction, max_reduction,
+        [=](nd_item<1> item, auto& min, auto& max) {
+                int idx = item.get_global_id(0);
+                min.combine(data[idx]);
+                max.combine(data[idx]);
+            });
+        }).wait();
+
+        //# STEP 3 : Compute mid_range from min and max
+        int mid_range = (*min + *max) / 2;
+        std::cout << "Mid-Range = " << mid_range << "\n";
+        free(data, q);
+        free(min, q);
+        free(max, q);
+        return 0;
+}
